44b4203c38980583a8977f1ecaabfead5d50db51
KamillaLima <kamillalima868@gmail.com>
2024-03-04 11:01:55 -0300

    projeto Conta Bancaria

---
+++ src/conta/Menu.java
@@ -0,0 +1,12 @@
+package conta;
+
+import conta.util.Cores;
+
+public class Menu {
+public static void main(String[] args) {
+	System.out.println("---MENU BANCO---");
+	
+	Cores cores = new Cores();
+	cores.mostrarPrint();
+}
+}
---
+++ src/conta/util/Cores.java
@@ -0,0 +1,8 @@
+package conta.util;
+
+public class Cores {
+	public void mostrarPrint() {
+		System.out.println("Mostrando o método");
+	}
+
+}
